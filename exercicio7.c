f7cdfe77cce0a041c98bcecd4832898843614766
Leandro Pinzon Filho <131808539+Lfijho@users.noreply.github.com>
2024-08-19 15:20:35 -0300

    Update exercicio7.c

--- exercicio7.c
+++ exercicio7.c
@@ -1,3 +1,10 @@
+//Ler um valor numérico que esteja na faixa de valores de 1 até 9. O programa deve apresentar a 
+//mensagem “O valor está na faixa permitida”, caso o valor informado esteja entre 1 e 9. Se o valor 
+//estiver fora dessa faixa, o programa deve apresentar a mensagem “O valor está fora da faixa 
+//permitida”.
+
+
+
 #include <stdio.h>
 
 int main(void) {
