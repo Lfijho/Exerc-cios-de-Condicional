8c6591393df471beca7c499d9db3ec586680d14b
Leandro Pinzon Filho <131808539+Lfijho@users.noreply.github.com>
2024-08-19 15:23:11 -0300

    Update exercicio2.c

--- exercicio2.c
+++ exercicio2.c
@@ -1,1 +1,3 @@
-
+//Escreva um algoritmo para ler um valor numérico inteiro positivo ou negativo e apresentar o valor 
+//lido como sendo um valor positivo, ou seja, se o valor lido for menor ou igual a zero, ele deve ser 
+//multiplicado por -1.
