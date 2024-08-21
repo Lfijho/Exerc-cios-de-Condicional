887b544749f674b880f4601b922b51672c3e0d9d
Leandro Pinzon Filho <131808539+Lfijho@users.noreply.github.com>
2024-08-21 13:50:36 -0300

    Update exercicio1.c

--- exercicio1.c
+++ exercicio1.c
@@ -1,1 +1,12 @@
 //Escreva um algoritmo para ler um valor e escrever se é positivo ou negativo. Considere o valor zero como positivo.
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void) {
+    int num;
+  scanf( "%d", &num);
+    if ( num < 0 ) printf("É negativo!");
+    else printf("É positivo!");
+
+}
