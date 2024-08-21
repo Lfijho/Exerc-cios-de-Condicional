2b841038f5d3a65e738719358fe13540870bb11a
Leandro Pinzon Filho <131808539+Lfijho@users.noreply.github.com>
2024-08-21 13:51:07 -0300

    Update exercicio2.c

--- exercicio2.c
+++ exercicio2.c
@@ -1,3 +1,13 @@
 //Escreva um algoritmo para ler um valor numérico inteiro positivo ou negativo e apresentar o valor 
 //lido como sendo um valor positivo, ou seja, se o valor lido for menor ou igual a zero, ele deve ser 
 //multiplicado por -1.
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void) {
+    int num;
+    scanf("%d", &num);
+    if (num <= 0 ) num = num * -1;
+    printf("%d\n", num);
+}
