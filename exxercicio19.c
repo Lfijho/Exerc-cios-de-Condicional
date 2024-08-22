670fa4d546b98acbe70966893b56c7702c1e1603
Leandro Pinzon Filho <131808539+Lfijho@users.noreply.github.com>
2024-08-22 14:58:49 -0300

    Update exxercicio19.c

--- exxercicio19.c
+++ exxercicio19.c
@@ -1,1 +1,31 @@
+//O programa recebe um caracter e imprime por extenso o estado civil.
+// C – Casado(a)
+// S- Solteiro(a)
+// D – Divorciado(a)
+// V- Viúvo(a)
 
+
+
+
+
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <ctype.h> /
+int main(void) {
+  char estadoCivil;
+  scanf("%c", &estadoCivil);
+  estadoCivil = toupper(estadoCivil); /
+  switch(estadoCivil) {
+    case 'S': printf("SOLTEIRO");
+    break;
+    case 'C': printf("CASADO");
+    break;
+    case 'D': printf("DIVORCIADO");
+    break;
+    case 'V': printf("VIÚVO");
+    break;
+  }
+  
+}
